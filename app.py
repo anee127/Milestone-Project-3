0b5a09877dadb7242607ebc1fe730a5eec9ed01a
Anee <aneesa.khan97@outlook.com>
2021-11-12 16:25:36 +0000

    linked edit recipe page to edit button

--- app.py
+++ app.py
@@ -126,13 +126,15 @@ def add_recipe():
         return redirect(url_for("add_recipe"))
     return render_template("add_recipe.html")
 
-@app.route("/edit_recipe/<recipes_id>", methods=["GET", "POST"])
-def edit_recipe(recipes_id):
-    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipes_id)})
+
+# Editing Recipe function
+@app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
+def edit_recipe(recipe_id):
+    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     return render_template("edit_recipe.html", recipe=recipe)    
 
 # Deleting Recipe function
-@app.route("/delete_recipe/<recipe_id>")
+@app.route("/delete_recipe/<recipes_id>")
 def delete_recipe(recipe_id):
     # Delete recipe from db
     mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
