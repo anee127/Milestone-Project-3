ac6264c0539c46494f974b22726a15dc72e16332
Anee <aneesa.khan97@outlook.com>
2021-11-29 17:18:17 +0000

    added jinja templating to recipe cards to render instructions, timing and servings

--- app.py
+++ app.py
@@ -150,8 +150,8 @@ def add_recipe():
     if request.method == "POST":
         recipe = {
             "recipe_name": request.form.get("recipe_name"),
-            "recipe_time": request.form.get("recipe_time"),
-            "recipe_serves": request.form.get("recipe_serves"),
+            "timing": request.form.get("timing"),
+            "servings": request.form.get("servings"),
             "ingredients": request.form.get("ingredients"),
             "instructions": request.form.get("instructions"),
             "image_url": request.form.get("image_url"),
@@ -173,8 +173,8 @@ def edit_recipe(recipe_id):
     if request.method == "POST":
         recipe_info = {
             "recipe_name": request.form.get("recipe_name"),
-            "recipe_time": request.form.get("recipe_time"),
-            "recipe_serves": request.form.get("recipe_serves"),
+             "timing": request.form.get("timing"),
+            "servings": request.form.get("servings"),
             "ingredients": request.form.get("ingredients"),
             "instructions": request.form.get("instructions"),
             "image_url": request.form.get("image_url"),
